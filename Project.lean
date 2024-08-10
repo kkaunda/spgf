d52dc7c0074578ab770817c1668bdda23c63cb18
kkaunda <kajanikaunda@gmail.com>
2024-08-10 20:55:13 +0200

    commit me - 1

--- Project.lean
+++ Project.lean
@@ -1,1 +1,18 @@
-import Project.Example
+--import Project.Example
+
+--#eval hi
+
+/-
+example (p q r : Prop) : ((p ∨ q) → r ) ↔ ((p → r) Λ (q → r)) :=
+begin
+ sorry
+end
+-/
+def getGreeting (name : String) := s!"Hello, {name}! isn't lean great!"
+
+def main : IO Unit :=
+   let names := ["john","mary","jane"]
+   let greetings := names.map getGreeting
+
+   for greeting in greetings do
+      IO.println greeting
